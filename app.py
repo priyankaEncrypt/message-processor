11ea84a58e3f3a274cb74fc442c6d3d8c005fb78
priyanka <priyankas2302@gmail.com>
2026-09-19 17:51:11 +0530

    Initial project Setup

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-print("Getting started with Git")
-print("Version 2")
+from urllib import request
+
+import streamlit as st
+import requests
+
+st.title("Message Processor")
+message=st.text_input("Enter your message")
+
+if st.button("Process message"):
+    response =request.post(
+    "http://localhost:8000/process",
+    params={"message": message}
+   )
+    result = response.json()
+    st.subheader("Result")
+    
+    st.write(result["message"])
+    st.write(result["length"])
+  
